874815b17c53495ca66856a76d3a9668ec8e0e66
Faezeh Mohammadi <59282324+faezehmhmdi@users.noreply.github.com>
2020-08-15 14:27:15 +0430

    Update database.sql
    added new attributes (supporter , ...) 
    added new functions
    edits are remaining

--- database.sql
+++ database.sql
@@ -1,31 +1,73 @@
 -- Hosts Table 
-create table hosts (
-	id			int auto_increment,
-    name        varchar(100)not null default 'unknown',
-    key (id),
-    primary key (name)
+create table Hosts (
+	id	int auto_increment,
+	name	varchar(100) not null,
+	manager	varchar(100) not null,
+	key (id),
+	primary key(name,manager)
 );
 
 -- Conferences Table
-create table conference (
-    id           varchar(20) not null,
+create table Conference (
+	id	int auto_increment,
+    request_number	varchar(100) not null,
 	request_sentDate	Date not null,
-    topic        varchar(100) not null,
-    start_Date   Date not null,
-    end_Date     Date not null,
-	start_time	 time (0) not null default '08:00:00',
-	end_time	 time (0) not null default '08:00:00',
-    host_n    	 varchar(100) not null,
-    url          varchar(2048) not null,
-	supporter_name	varchar(100) not null, -- also we need contact information
-	platform_type	varchar(100) not null, -- skype, webinar and ...
-	isCanceled	bit default 'false',
-    primary key (id, topic),
-	constraint times check (start_time <= end_time),
-	constraint dates check (start_Date <= end_Date ),
-    foreign key (host_n) references hosts (name) 
+    topic	varchar(100) not null,
+    start_Date	Date not null,
+    end_Date	Date not null,
+	start_time	time (0) not null default '08:00:00',
+	end_time	time (0) not null default '08:00:00',
+	placeId	int,
+    hostId	int,
+	platformId	int,	
+	supporterId	int,
+	isCanceled	boolean default 0,
+	isHost	boolean	default	1, --if 0 we are guest if 1 we are host
+    key(id),
+	primary key(request_number, topic),
+	check (start_time <= end_time),
+	check (start_Date <= end_Date ),
+    foreign key (hostId) references Hosts (id)
 	on update cascade 
+	on delete cascade,
+	foreign key (supporterId) references Supporter (id)
+	on update cascade 
+	on delete cascade,
+	foreign key (platformId) references Platform (id) 
+	on update cascade
+	on delete cascade,
+	foreign key (placeId) references Place (id)
+	on update cascade
 	on delete cascade
+);
+
+--Supporter TABLE
+create table Supporter (
+	id int auto_increment,
+	name	varchar(100) not null,
+	telephone varchar(11) not null,
+	key(id),
+	primary key(id, name),
+	constraint valid_telephone_number check (telephone REGEXP '^09[0-9]{9}$')
+);
+
+--platform Table
+create table Platform (
+	id int auto_increment,
+	name	varchar(100) not null,
+	url	varchar(2048) not null,
+	description	varchar(512) not null,
+	key (id),
+	primary key(id, name)
+);
+
+--Place Table
+create table Place (
+	id int auto_increment,
+	name	varchar(100) not null,
+	isEmpty	boolean not null default 1,
+	key(id),
+	primary key(id, name)
 );
 
 -- Admin Account Table
@@ -38,23 +80,13 @@ create table AdminAcc (
 -- Participators Table
 create table participator (
     id          int auto_increment,  
-    fname       varchar(100) not null,
-	lname		varchar(100) not null,
-    email       varchar(100) not null,
-	conf_id		varchar(20) not null,
-    telephone   varchar(11) not null,
+    name	varchar(100) not null,
+	confId		int,
     key (id),
-	primary key (fname, lname),
+	primary key (fnam),
 	foreign key (conf_id) references conference (id) on update cascade on delete cascade
 );
 
--- Guests TABLE
-create table guests (
-	id		int auto_increment,
-	fname	varchar(100) not null,
-	lname	varchar(100) not null,
-	primary key(id,fname,lname)
-);
 
 -- Login Table
 create table logins (
@@ -64,26 +96,120 @@ create table logins (
     foreign key (username) references AdminAcc (username) on delete cascade
 );
 
--- Add Conference Function and Host info
+--Add Host Function 
 delimiter //
-create function addConference (
-    id varchar(20), topic varchar(100), start_Date Date, end_Date Date, start_time time (0), end_time time (0), host_n varchar(100), url varchar(100))
-    returns char(64)
-    begin
+create function addHost (
+	hostName varchar(100), manager varchar(100))
+	returns char(64)
+	begin
 	declare res char(64);
-    		if (select count(*) from hosts as t where t.name=host_n) = 0 then
-				insert into hosts values ('', host_n);
-			end if;
-			if (select count(*) from conference as c where (c.id=id) and (c.topic=topic) and (c.start_Date=start_Date) and (c.end_Date=end_Date) and (c.start_time=start_time) and (c.end_time=end_time) and (c.host_n=host_n)) = 0 then
-				insert into conference values (id, topic, start_Date, end_Date, start_time, end_time, host_n, url);
-				set res = 'Data is saved';
-			else
-				set res = 'Data Duplication';
-			end if;
-			return res;
+	if(select count(*) from Hosts as h where (h.name = hostName) and (h.manager = manager)) = 0 then
+		insert into Hosts values ('',hostName, manager);
+		set res = 'Host added.';
+	else
+		set res = 'Host with the same manager exists.';
+	end if;
+	return res;
 end
 //
 delimiter ;
+
+-- Edit Host info Function
+delimiter //
+create function editHost (
+	name varchar(100) ,manager varchar(100))
+	returns char(64)
+	begin
+	declare res char(64)
+	if ()
+end
+//
+delimiter ;
+
+-- Add Supporter Function
+delimiter //
+create function addSupporter (
+	SupporterName varchar(100), telephone varchar(11))
+	returns char(64)
+	begin
+	declare res char(64);
+	if(select count(*) from Supporter as s where (s.name = supporterName) and (s.telephone = telephone)) = 0 then
+		insert into Supporter values ('', supporterName, telephone);
+		set res = 'Supporter added.';
+	else
+		set res = 'Supporter with same telephone exists.';
+	end if;
+	return res;
+end
+//
+delimiter ;
+
+-- Add Platform Function
+delimiter //
+create function addPlatform (
+	platName varchar(100),url varchar(2048),description	varchar(512))
+	returns char(64)
+	begin
+	declare res char(64);
+	if (select count(*) from Platform as p where (p.name = platName) and (p.url = url) and (p.description = description)) = 0 then
+		insert into Platform values ('', platName, url, description);
+		set res = 'Platform added.';
+	else
+		set res = 'Already exists.';
+	end if;
+	return res;
+end
+//
+delimiter ;
+
+/* -- fix defaul beging 1
+-- fix defaul beging 1
+-- fix defaul beging 1*/
+-- Add Place Function
+delimiter //
+create function addPlace (
+	placName varchar(100), isEmp boolean)
+	returns char(64)
+	begin
+	declare res char(64);
+	if (select count(*) from Place as p where (p.name = placName)) = 0 then
+		insert into Place values ('', placName, coalesce(isEmp, 1));
+		set res = 'Place added.';
+	else
+		update Place as p set p.isEmpty = isEmp where p.name = placName;
+		set res = 'Updated.';
+	end if;
+	return res;
+end
+//
+delimiter ;
+
+
+-- Add Conference and Supporter and platform and place and host info in their tables
+delimiter //
+create function addAllDetails (
+	request_number varchar(100), request_sentDate Date, topic varchar(100), start_Date Date, end_Date Date, start_time time (0),
+	end_time time (0),isCanceled bit default 0, isHost bit default 0, hostName varchar(100), manager varchar(100),supporterName varchar(100),
+	telephone varchar(11), platformName varchar(100),url varchar(2048), description varchar(512), placeName varchar(100), isEmpty bit default 1)
+	returns char(64)
+	begin
+	declare res char(64);
+	insert into Hosts values (hostName, manager);
+	insert into Platform values (platformName, url, description);
+	if (select count(*) from Supporter as s where (s.name = supporterName) and (s.telephone = telephone)) = 0 then
+		insert into Supporter values (supporterName, telephone);
+	else
+		set res = 'Supporter Exists';
+	if (select count(*) from Place as plac where plac.name = placeName) = 0 then
+		insert into Place values (placeName, isEmpty);
+	else
+		update Place as p set p.isEmpty = isEmpty where plac.name = placeName;
+	if (select count(*) from Conference as c from)
+		
+end
+//
+delimiter :
+
 
 -- Login Admin Function
 delimiter //
